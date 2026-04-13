eb7e865b89da3b89a2476ef84e33a0ca47aa218f
Ritik Raj <rajritik2425@gmail.com>
2026-04-13 15:29:01 +0530

    It's shows plane health now

--- src/hud.h
+++ src/hud.h
@@ -3,7 +3,7 @@
 
 void hud_init(void);
 void hud_update(void);
-void hud_draw(void);
+void hud_draw(int health);   /* renders SCORE + health bar each frame */
 int  hud_get_score(void);
 
 #endif /* HUD_H */
